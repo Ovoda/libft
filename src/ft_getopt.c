91d1abe1a212db248f31de7a6b089e80baa884ef
Calixte DE TOURRIS <cdetourrispro@gmail.com>
2023-08-27 19:23:17 +0200

    chore(ft_getopt): minimal viable version of getopt

--- src/ft_getopt.c
+++ src/ft_getopt.c
@@ -6,7 +6,7 @@
 /*   By: calide-n <calide-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 20:46:54 by calide-n          #+#    #+#             */
-/*   Updated: 2023/08/27 18:27:48 by calide-n         ###   ########.fr       */
+/*   Updated: 2023/08/27 19:19:41 by calide-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 // TODO - add POSIXLY_CORRECT feature
 // TODO - add long options feature
 // TODO - add inetutils-2.0 non global opt args feature for re entrant usecase (usefull for ping & nmap)
+// TODO - add optstring order feature
 
 char *ft_optarg = NULL;
-int ft_optopt;
+int ft_optopt = 0;
 int ft_opterr = 1;
 int ft_optind = 1;
 
@@ -36,7 +37,7 @@ static char *ft_optstring_index(const char *str, char c) {
 }
 
 int ft_getopt(int argc, char *const *argv, char const *optstring) {
-    (void)optstring;
+    ft_optarg = NULL;
 
     // If nextchar not set, set nextchar
     if (nextchar == NULL || *nextchar == '\0') {
@@ -45,27 +46,32 @@ int ft_getopt(int argc, char *const *argv, char const *optstring) {
         if (ft_optind == argc || ft_strcmp(argv[ft_optind], "--") == 0) {
             return -1;
         }
-        // TODO - Check if argument is a non-option, and if so, set ft_optarg
-        // TODO - Set nextchar
+        // Check if argument is option
+
+        int nonoption = argv[ft_optind][0] != '-' || argv[ft_optind][1] == '\0';
+        if (nonoption) {
+            return -1;
+        }
         nextchar = argv[ft_optind] + 1;
     }
+
 
     // TODO - handle current char, fetch its index from optstring
     char c = *nextchar++;
     char *tmp = ft_optstring_index(optstring, c);
 
+    // Increment ft_optind if we finished with current option
+    if (*nextchar == '\0') {
+        ft_optind++;
+    }
+
     // TODO - check for error (c not in optstring or c == ':')
     if (tmp == NULL) { // || c == ':') {
         if (opterr) {
             fprintf(stderr, "%s: illegal option -- %c\n", argv[0], c);
-            exit(1);
         }
-        return -1;
-    }
-
-    // Increment ft_optind if we finished with current option
-    if (*nextchar == '\0') {
-        ft_optind++;
+        ft_optopt = (int)c;
+        return '?';
     }
 
     // Check for option arguments
@@ -85,7 +91,7 @@ int ft_getopt(int argc, char *const *argv, char const *optstring) {
                 fprintf(stderr, "%s: option requires an argument -- %c\n", argv[0], c);
             }
             // TODO add c = ':' when ':' present in optstring
-            ft_optopt = c;
+            ft_optopt = (int)c;
             c = '?';
         }
         else {
@@ -102,10 +108,13 @@ int ft_getopt(int argc, char *const *argv, char const *optstring) {
 // -> clang ft_getopt.c ../libft.a -L../ -I ../ -DTEST && ./a.out -a
 
 int main(int argc, char *const *argv) {
-    char c = getopt(argc, argv, "a:");
-    printf("%c, %s\n", c, optarg);
-    char d = ft_getopt(argc, argv, "a:");
-    printf("%c, %s\n", d, ft_optarg);
+    int c, d;
+
+    while ((c = getopt(argc, argv, "a:bc")) != -1 && (d = ft_getopt(argc, argv, "a:bc")) != -1) {
+        printf("%c, %s %d %d\n", c, optarg, optopt, opterr);
+        printf("%c, %s %d %d\n", d, ft_optarg, ft_optopt, ft_opterr);
+    }
+
     return 0;
 }
 
